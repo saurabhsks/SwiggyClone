1cd838b6916d04ebc88bc7ab08e3e7774d956ba2
saurabhkeyideas <saurabhkumar.singh@keyideasglobal.com>
2025-01-31 21:08:23 +0530

    header & left slider is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import Header from './components/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='text-3xl'>React Setup is ready Saurabh</div>
+    <Header/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,86 @@
+import React, {useState} from 'react';
+import { RxCaretDown } from "react-icons/rx";
+import { IoMdSearch } from "react-icons/io";
+import { CiDiscount1 } from "react-icons/ci";
+import { LiaHandsHelpingSolid } from "react-icons/lia";
+import { LiaSignInAltSolid } from "react-icons/lia";
+import { PiShoppingCartFill } from "react-icons/pi";
+
+
+export default function Header() {
+    const [toggle, setToggle] = useState(false);
+    const showSideMenu = () =>{
+        setToggle(true);
+    }
+    const hideSideMenu = () =>{
+        setToggle(false);
+    }
+    const links =[
+        {
+            icon:<IoMdSearch />,
+            name: "Search"
+        } ,
+        {
+            icon:<CiDiscount1/>,
+            name: "Offers",
+            sup:"New"
+        } ,
+        {
+            icon:<LiaHandsHelpingSolid />,
+            name: "Help"
+        } ,
+        {
+            icon:<LiaSignInAltSolid />,
+            name: "SignIn"
+        } ,
+        {
+            icon:<PiShoppingCartFill />,
+            name: "Cart"
+        }
+    ]
+
+
+  return (
+    <>
+     <div className='black-overlay w-full h-full fixed duration-500' onClick={hideSideMenu} style={{
+        opacity:toggle ? 1:0,
+        visibility:toggle ? "visible" : "hidden"
+     }}>
+        <div onClick={(e)=>{
+            e.stopPropagation(); 
+        }} className='w-[400px] bg-white h-full absolute duration-[400ms]'
+        style={{
+            left:toggle ? '0%': '-100%'
+        }}
+        ></div>
+     </div>
+
+
+    <header className='p-[15px] shadow-xl'>
+      <div className='max-w-[1200px] mx-auto flex items-center'>
+      <div className='w-[100px]'>
+        <img src="src/assets/logo.png" className='w-full' alt=""/>
+      </div>
+      <div className=''>
+        <span className='font-bold border-b-[3px] border-[black] mr-3'>Ratanada</span>
+         Jodhpur,Rajasthan, India <RxCaretDown onClick={showSideMenu} fontSize={25} className='cursor-pointer font-bold inline text-[#fc8019]'/>
+      </div>
+      <nav className='flex list-none gap-10 ml-auto text-[17px] font-semibold'>
+       {
+          links.map(
+            (link,index)=>{
+                return <li key={index} className='flex hover:text-[#fc8019] cursor-pointer items-center gap-2'>
+                        {link.icon}
+                        {link.name}
+                        <sup>{link.sup}</sup>
+                </li>
+            }
+          )
+       }
+       
+      </nav>
+      </div>
+    </header>
+    </>
+  )
+}
