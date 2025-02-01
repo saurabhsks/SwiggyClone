bcfd663d82482e932c03dd18d90a7a42d23ddb25
saurabhkeyideas <saurabhkumar.singh@keyideasglobal.com>
2025-02-01 22:12:13 +0530

    mobile view fix

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -65,7 +65,7 @@ export default function Category() {
                      style={{
                         transform: `translateX(-${slide*100}%)`
                     }} 
-                    key={index} className='group w-[150px] shrink-0 duration-500'>
+                    key={index} className='group w-[150px] cursor-pointer shrink-0 duration-500'>
                         {/* <img src={"http://localhost:5000/images/" + cat.image} alt="" /> */}
                         <img src={"https://swiggyapis-ctp4.onrender.com/images/" + cat.image} alt="" className='group-hover:scale-110 duration-150' />
                     </div>
